475d04840343773d106836261641232d258c0445
Ice <icebenitez@gmail.com>
2024-03-23 23:35:43 +0800

    removed swal for adding new pet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { doc } from "firebase/firestore";
 import {
   Container,
   Row,
@@ -7,122 +8,30 @@ import {
   Card,
   Form,
   Spinner,
+  Stack,
 } from "react-bootstrap";
-import { doc } from "firebase/firestore";
+import { Link } from "react-router-dom";
 
 import useDocOnSnapshot from "../utils/hooks/useDocOnSnapshot";
 import useAuth from "../utils/hooks/useAuth";
 import { db } from "../utils/firebase";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
-
-const ReactSwal = withReactContent(Swal);
 
 const MainPage = () => {
-  const { user } = useAuth();
+  const uid = useAuth()?.user.uid;
   const { data: userDetails, loading } = useDocOnSnapshot(
-    doc(db, "Users", `${user?.uid}`)
+    doc(db, "Users", `${uid}`)
   );
-
-  // Sample pet data for testing
-  const [petData, setPetData] = useState({
-    picture: "",
-    name: "",
-    dateOfBirth: "",
-    vaccines: "",
-  });
-
-  const openAddPetPopup = () => {
-    ReactSwal.fire({
-      title: "Add Pet",
-      html: (
-        <Form>
-          <Form.Group className="mb-3" controlId="formPetPicture">
-            <Form.Label>Pet Picture</Form.Label>
-            <Form.Control
-              type="file"
-              name="picture"
-              onChange={handlePetDataChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formPetName">
-            <Form.Label>Pet Name</Form.Label>
-            <Form.Control
-              type="text"
-              name="name"
-              value={petData.name}
-              onChange={handlePetDataChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formPetDateOfBirth">
-            <Form.Label>Date of Birth</Form.Label>
-            <Form.Control
-              type="date"
-              name="dateOfBirth"
-              value={petData.dateOfBirth}
-              onChange={handlePetDataChange}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formPetVaccines">
-            <Form.Label>Vaccines</Form.Label>
-            <Form.Control
-              type="text"
-              name="vaccines"
-              value={petData.vaccines}
-              onChange={handlePetDataChange}
-            />
-          </Form.Group>
-        </Form>
-      ),
-      showCancelButton: true,
-      confirmButtonText: "Add Pet",
-      showLoaderOnConfirm: true,
-      preConfirm: () => handleAddPet(),
-    });
-  };
-
-  // Function to handle adding a new pet
-  const handleAddPet = () => {
-    const { picture, name, dateOfBirth, vaccines } = petData;
-    // Validate pet data
-    if (!picture || !name || !dateOfBirth || !vaccines) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Please fill out all fields!",
-      });
-      return;
-    }
-
-    // Create a new pet object
-    const newPet = { ...petData };
-    // setUserDetails((prevState) => ({
-    //   ...prevState,
-    //   pets: [...prevState.pets, newPet],
-    // }));
-    // Reset pet data for the next addition
-    setPetData({
-      picture: "",
-      name: "",
-      dateOfBirth: "",
-      vaccines: "",
-    });
-    // Close the SweetAlert popup
-    Swal.close();
-  };
-
-  // Function to handle input changes for pet data
-  const handlePetDataChange = (e) => {
-    const { name, value } = e.target;
-    setPetData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+  const [pets, setPets] = useState([]);
 
   return (
     <Container className="mt-5">
-      <h2>User Details</h2>
+      <Stack direction="horizontal" gap={3}>
+        <h2>User Details</h2>
+        <Button variant="primary" className="mb-3 ms-auto">
+          Edit Profile
+        </Button>
+      </Stack>
+
       {loading ? ( // If loading, display spinner
         <Spinner animation="border" role="status">
           <span className="visually-hidden">Loading...</span>
@@ -134,19 +43,26 @@ const MainPage = () => {
             <p>Last Name: {userDetails.lastName}</p>
             <p>Address: {userDetails.address}</p>
             <p>Contact Number: {userDetails.contactNumber}</p>
-
-            <Button variant="primary" className="mb-3">
-              Edit Profile
-            </Button>
           </>
         )
       )}
 
       {!loading && userDetails && (
         <>
-          <h2 className="mt-4">Pets</h2>
+          <Stack direction="horizontal" gap={3}>
+            <h2 className="mt-4">Pets</h2>
+            <Button
+              variant="primary"
+              className="mt-3 ms-auto"
+              as={Link}
+              to="/pet/new"
+            >
+              Add Pet
+            </Button>
+          </Stack>
+
           <Row xs={1} md={2} lg={3} className="g-4">
-            {userDetails?.pets?.map((pet, index) => (
+            {pets?.map((pet, index) => (
               <Col key={index}>
                 <Card>
                   <Card.Img variant="top" src={pet?.picture} />
@@ -162,10 +78,6 @@ const MainPage = () => {
               </Col>
             ))}
           </Row>
-
-          <Button variant="primary" className="mt-3" onClick={openAddPetPopup}>
-            Add Pet
-          </Button>
         </>
       )}
     </Container>
--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -24,34 +24,10 @@ const router = createBrowserRouter([
           {
             path: "/",
             element: <Home />,
-            // children: [
-            //   {
-            //     path: "contact",
-            //     element: <Contact />,
-            //   },
-            //   {
-            //     path: "dashboard",
-            //     element: <Dashboard />,
-            //     loader: ({ request }) =>
-            //       fetch("/api/dashboard.json", {
-            //         signal: request.signal,
-            //       }),
-            //   },
-            //   {
-            //     element: <AuthLayout />,
-            //     children: [
-            //       {
-            //         path: "login",
-            //         element: <Login />,
-            //         loader: redirectIfUser,
-            //       },
-            //       {
-            //         path: "logout",
-            //         action: logoutUser,
-            //       },
-            //     ],
-            //   },
-            // ],
+          },
+          {
+            path: "pet",
+            children: [],
           },
         ],
       },
