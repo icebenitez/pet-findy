fd00c48ecc6da46e1ff96bb4689b1b4721a7a505
HisExcellency <80304375+icebenitez@users.noreply.github.com>
2024-04-03 13:41:17 +0800

    feat: add contactDetails and address in pet form (#14)

--- src/components/ProtectedPages.jsx
+++ src/components/ProtectedPages.jsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
-import { Outlet, useNavigate, useOutletContext } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import useAuth from "../utils/hooks/useAuth";
 
 const Protected = () => {
-  const { user, loading } = useOutletContext();
+  const { user, loading } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user?.uid) {
+    if (!loading && !user?.uid) {
       navigate("/login");
     }
-  }, [user, navigate]);
+  }, [user, loading, navigate]);
 
   return (
     <>
-      <Outlet context={{ user, loading }} />
+      <Outlet />
     </>
   );
 };
