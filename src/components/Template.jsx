e0a2b47896ae6c1fafae279a96cbb49d11b3e1c3
Ice <icebenitez@gmail.com>
2024-04-03 13:30:27 +0800

    feat: add contactDetails and address in pet form

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import useAuth from "../utils/hooks/useAuth";
 
 const Template = () => {
-  const { user, loading } = useAuth();
-
   return (
     <>
       <Navbar />
-      <Outlet context={{ user, loading }} />
+      <Outlet />
     </>
   );
 };
--- src/pages/Pet.jsx
+++ src/pages/Pet.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams, Link, useOutletContext } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { doc, onSnapshot } from "firebase/firestore";
-import { db } from "../utils/firebase";
 import { Button, Container, Spinner } from "react-bootstrap";
+
+import useAuth from "../utils/hooks/useAuth";
+import { db } from "../utils/firebase";
 
 const Pet = () => {
   const { userId, petId } = useParams();
-  const { user } = useOutletContext();
+  const { user } = useAuth();
   const [pet, setPet] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -18,7 +20,7 @@ const Pet = () => {
         setLoading(false);
       }
     );
-    
+
     return () => unsubscribe();
   }, [userId, petId]);
 
@@ -48,6 +50,8 @@ const Pet = () => {
           </div>
         )}
         {pet.dateOfBirth && <p>Date of Birth: {pet.dateOfBirth}</p>}
+        {pet?.contactDetails && <p>Contact Details: {pet.contactDetails}</p>}
+        {pet?.address && <p>Address: {pet.address}</p>}
         {/* Display other pet details here */}
       </div>
       {user?.uid ? (
@@ -55,7 +59,7 @@ const Pet = () => {
           Edit
         </Button>
       ) : (
-        <p>To edit this pet's details, you must be authenticated.</p>
+        <p>Please sign in to edit this pet's details.</p>
       )}
     </Container>
   );
